73bfe7bc0032ae9a43dcc1d7d2eadb78e8961365
ShutterFox <shutterfox5555@gmail.com>
2015-01-18 12:19:24 +0000

    MPG Controller

---
+++ SilverProjects/SilverProjects/Controllers/MPGController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SilverProjects.Controllers
+{
+    using System.Dynamic;
+
+    using SilverProjects.Models;
+
+    public class MPGController : Controller
+    {
+        // GET: MPG
+        public ActionResult Index()
+        {
+            var viewModel = new MPGIndexViewModel();
+            viewModel.Bob = "butts";
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ SilverProjects/SilverProjects/Models/MPGIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SilverProjects.Models
+{
+    public class MPGIndexViewModel
+    {
+        public decimal CostPerL { get; set; }
+
+        public decimal CostPumped { get; set; }
+
+        public decimal Miles { get; set; }
+
+        public string Bob { get; set; }
+    }
+}
