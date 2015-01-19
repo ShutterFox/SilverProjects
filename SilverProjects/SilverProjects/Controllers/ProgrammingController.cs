c8f009ca3dce4bea04a9f2887d1c52bfd38e4715
ShutterFox <x@x.com>
2015-01-19 21:03:12 +0000

    Site page structure & Content
    Altered the main index content, and created index pages for the 3
    sections the site will have

---
+++ SilverProjects/SilverProjects/Controllers/ProgrammingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SilverProjects.Controllers
+{
+    public class ProgrammingController : Controller
+    {
+        // GET: Programming
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
