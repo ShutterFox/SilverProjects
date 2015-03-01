1497055dceddf6cbfe08ab2b7dac7b66b744bb61
ShutterFox <shutterfox5555@gmail.com>
2015-03-01 13:48:19 +0000

    Created Photos database
    recreated the Photos database, with filename as a field instead of HTML
    code. Attempting to get it to work this way instead.

---
+++ SilverProjects/SilverProjects/Models/Photos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SilverProjects.Models
+{
+    public class Photos
+    {
+        public int ID { get; set; }
+        public string FileName { get; set; }
+        public string Tags { get; set; }
+        public String Category { get; set; }
+
+    }
+
+    public class PhotosDBContext : DbContext
+    {
+        public DbSet<Photos> Photos { get; set; }
+    }
+}
